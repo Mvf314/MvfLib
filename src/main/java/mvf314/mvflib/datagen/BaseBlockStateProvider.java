f243aa269325c9bd819953e8f6caf6444ddc1a8e
Mvf314 <mvf314@gmail.com>
2020-08-20 23:14:23 +0200

    Added classes for directional blocks in the X-Z plane, so only horizontal direction

--- src/main/java/mvf314/mvflib/datagen/BaseBlockStateProvider.java
+++ src/main/java/mvf314/mvflib/datagen/BaseBlockStateProvider.java
@@ -2,6 +2,7 @@ package mvf314.mvflib.datagen;
 
 import mvf314.mvflib.block.BaseBlock;
 import mvf314.mvflib.block.DirectionalBlock;
+import mvf314.mvflib.block.DirectionalXZBlock;
 import mvf314.mvflib.setup.RegistryMap;
 import net.minecraft.block.BlockState;
 import net.minecraft.data.DataGenerator;
@@ -14,6 +15,7 @@ import net.minecraftforge.client.model.generators.ExistingFileHelper;
 import net.minecraftforge.client.model.generators.ModelFile;
 
 import javax.annotation.Nonnull;
+import java.security.InvalidParameterException;
 import java.util.function.Function;
 
 /**
@@ -54,18 +56,33 @@ public abstract class BaseBlockStateProvider extends BlockStateProvider {
 		simpleBlock(block, model);
 	}
 
-	protected void createDirectionalModelBlockstate(DirectionalBlock block, ModelFile modelFile) {
+	// can be used with either a DirectionalBlock of DirectionalXZBlock
+	protected void createDirectionalModelBlockstate(BaseBlock block, ModelFile modelFile) {
 		ModelFile model = modelFile;
 		Function<BlockState, ModelFile> modelFunc = $ -> model;
-		getVariantBuilder(block)
-				.forAllStates(state -> {
-					Direction dir = state.get(BlockStateProperties.FACING);
-					return ConfiguredModel.builder()
-							.modelFile(modelFunc.apply(state))
-							.rotationX(dir == Direction.DOWN ? 90 : (dir == Direction.UP ? -90 : 0))
-							.rotationY(dir == Direction.EAST ? 90 : (dir == Direction.SOUTH ? 180 : (dir == Direction.WEST ? 270 : 0)))
-							.build();
-				});
+		if (block instanceof DirectionalBlock) {
+			getVariantBuilder(block)
+					.forAllStates(state -> {
+						Direction dir = state.get(BlockStateProperties.FACING);
+						return ConfiguredModel.builder()
+								.modelFile(modelFunc.apply(state))
+								.rotationX(dir == Direction.DOWN ? 90 : (dir == Direction.UP ? -90 : 0))
+								.rotationY(dir == Direction.EAST ? 90 : (dir == Direction.SOUTH ? 180 : (dir == Direction.WEST ? 270 : 0)))
+								.build();
+					});
+		} else if (block instanceof DirectionalXZBlock) {
+			getVariantBuilder(block)
+					.forAllStates(state -> {
+						Direction dir = state.get(BlockStateProperties.HORIZONTAL_FACING);
+						return ConfiguredModel.builder()
+								.modelFile(modelFunc.apply(state))
+								.rotationX(dir == Direction.DOWN ? 90 : (dir == Direction.UP ? -90 : 0))
+								.rotationY(dir == Direction.EAST ? 90 : (dir == Direction.SOUTH ? 180 : (dir == Direction.WEST ? 270 : 0)))
+								.build();
+					});
+		} else {
+			throw new InvalidParameterException("Only pass directional block object to createDirectionalModelBlockstate!");
+		}
 	}
 
 	/**
@@ -78,7 +95,7 @@ public abstract class BaseBlockStateProvider extends BlockStateProvider {
 	 * @param left  Suffix for texture for left side
 	 * @param right Suffix for texture for right side
 	 */
-	protected void createDirectionalBlockstate(DirectionalBlock block, String up, String down, String front, String back, String left, String right) {
+	protected void createDirectionalBlockstate(BaseBlock block, String up, String down, String front, String back, String left, String right) {
 		String name = map.getValue(block);
 		ResourceLocation locUp = modLoc("block/" + name + up);
 		ResourceLocation locDown = modLoc("block/" + name + down);
